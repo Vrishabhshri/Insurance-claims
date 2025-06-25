0e3a8bcca0d31af24cb57e62b125b6e67b9cd348
Vrishabhshri <vrishabhshri@gmail.com>
2025-06-25 19:31:29 -0400

    Adding submit feature to ClaimService class

--- src/com/fineos/ClaimService.java
+++ src/com/fineos/ClaimService.java
@@ -4,6 +4,32 @@ import java.sql.*;
 
 public class ClaimService {
 
-    
+    private Connection conn;
+
+    public ClaimService(String dbPath) throws SQLException {
+
+        conn = DriverManager.getConnection("jdbc:sqlite" + dbPath);
+
+    }
+
+    public void submitClaim(String name, String type, double amount) throws SQLException {
+
+        String sql = "INSERT INTO claims (claimant_name, claim_type, amount) VALUES(?, ?, ?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, name);
+            stmt.setString(2, type);
+            stmt.setDouble(3, amount);
+            stmt.executeUpdate();
+
+        }
+
+    }
+
+    public void updateClaimStatus(int id, String status) throws SQLException {
+
+        
+
+    }
     
 }

aee43db1d823120f8e230fcacd25e97d6a4a41ff
Vrishabhshri <vrishabhshri@gmail.com>
2025-06-25 20:09:37 -0400

    Updating build.sh for my version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p out
-javac -d out -cp ".:sqlite-jdbc-3.36.0.3.jar" src/com/fineos/*.java
-java -cp "out:sqlite-jdbc-3.36.0.3.jar" com.fineos.Main
+javac -d out -cp ".:sqlite-jdbc-3.50.1.0.jar" src/com/fineos/*.java
+java -cp "out:sqlite-jdbc-3.50.1.0.jar" com.fineos.Main
